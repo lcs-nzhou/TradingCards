7f6204272c50e161d90b98310cfe7cfa5c9227e8
Nina Zhou <nzhou@lcs.on.ca>
2023-11-14 14:52:47 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Shuyu Zhou on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
