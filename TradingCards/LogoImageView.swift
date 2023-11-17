935f5b51316157fbd01fef05bcf607dce4163510
Nina Zhou <nzhou@lcs.on.ca>
2023-11-16 19:47:16 -0500

    initial commit

---
+++ TradingCards/LogoImageView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoImageView.swift
+//  TradingCards
+//
+//  Created by Shuyu Zhou on 2023-11-14.
+//
+
+import SwiftUI
+
+struct LogoImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoImageView()
+}
